205653f31a2c001650f4565ad2244694dc9af68e
kong <tee0918831415@gmail.com>
2026-01-31 16:57:58 +0700

    fix navbar addbook

--- public/js/library.js
+++ public/js/library.js
@@ -15,7 +15,17 @@ async function loadBooks() {
            alt="${book.title}" 
            style="height:200px;object-fit:cover">
 
-      <p class="mt-2">${book.title}</p>
+      <p class="mt-2 fw-bold">${book.title}</p>
+
+      <p class="text-muted small mb-2"
+         style="
+           display:-webkit-box;
+           -webkit-line-clamp:2;
+           -webkit-box-orient:vertical;
+           overflow:hidden;
+         ">
+        ${book.detail || "-"}
+      </p>
 
       <div class="d-flex gap-2 justify-content-center">
         <button class="btn btn-warning btn-sm" onclick="editBook('${book._id}')">
@@ -32,7 +42,6 @@ async function loadBooks() {
   });
 }
 
-
 function editBook(id) {
   window.location.href = "/addbook.html?id=" + id;
 }
@@ -44,6 +53,5 @@ async function deleteBook(id) {
 
   document.getElementById(`book-${id}`)?.remove();
 }
-
 
 loadBooks();
